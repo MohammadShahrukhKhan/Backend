8a893f35eb187b0c7cebfc43c514a3d2263ca4a5
Mohammad Shahrukh Khan <102281342+MohammadShahrukhKhan@users.noreply.github.com>
2022-08-03 14:11:45 +0530

    Update getting-started.js

--- MongoDB/Mongoose/getting-started.js
+++ MongoDB/Mongoose/getting-started.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/shark');
+  await mongoose.connect('mongodb://localhost:27017/shark'); // " shark " is the name of database
   console.log('connected')
 }
 
